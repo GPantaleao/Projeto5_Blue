abc12191f98f010d068244140965e6d4712ef98f
Natasha Miyaguti <naty.miyaguti@gmail.com>
2021-07-03 18:55:46 -0300

    introducao/ inicio nova fase

---
+++ fases/fase1.py
@@ -0,0 +1,2 @@
+def fase1(relogio, personagem):
+    print("São 8AM(horario), voce esta em um terreno baldio, qual sera sua ação:")
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from classes.personagemCao import Personagem
 from classes.relogio import Relogio
+from fases.introducao import Introducao
+from fases.fase1 import fase1
 
 if __name__ == '__main__':
-    pass
+    intro = Introducao()
+    intro.executar()
+    relogio = Relogio()
+    personagem = Personagem('')
+    fase1(relogio, personagem)
+
