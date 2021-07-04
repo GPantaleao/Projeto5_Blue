938d0e91051585b7c5c6954f1616b04401b698b1
Natasha Miyaguti <naty.miyaguti@gmail.com>
2021-07-04 00:57:15 -0300

    fase1

--- classes/personagemCao.py
+++ classes/personagemCao.py
@@ -7,13 +7,18 @@ class Personagem:
         self.fome = 50
         self.frio = 50
         self.energia = 50
+        self.lugar = 'terreno baldio'
 
     def __str__(self):          #altera os atributos confome necessidade, sendo valores positivos ou negativos#
         return f'''
-        Humor - {self.humor}   
-        Fome - {self.fome}    
-        Frio - {self.frio} 
-        Energia - {self.energia}    
+
+        Perfil {self.nome}:
+        ==============================
+        |{("Humor - " + str(self.humor) + "%").center(28)}|   
+        |{("Fome - " + str(self.fome) + "%").center(28)}|   
+        |{("Frio - " + str(self.frio) + "%").center(28)}|  
+        |{("Energia - " + str(self.energia) + "%").center(28)}|  
+        ==============================   
         '''
 
     def muda_humor(self,humor_novo):
@@ -27,6 +32,9 @@ class Personagem:
 
     def muda_energia(self,nova_energia):
        self.energia += nova_energia
+
+    def muda_lugar(self,novo_lugar):
+       self.lugar = novo_lugar 
 
     def atualizacao_frio(self,relogio,frio = -5):
         if relogio.noite_fria():
