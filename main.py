1e16a060c9e82d4e823c9b56db871bb53f08eb8d
SSilvaGustavo <gustavo.s.silva651@gmail.com>
2021-07-05 20:37:35 -0300

    Privando todas os metodos de todas as funções disponiveis

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-
+from classes.carrocinha import Carrocinha
 from classes.personagemCao import Personagem
 from classes.relogio import Relogio
+from classes.carrocinha import Carrocinha
 from fases.introducao import Introducao
 from fases.fases import fase1
 
 if __name__ == '__main__':
     intro = Introducao()
-    # intro.executar()   NÃO ESQUECER DE TIRAR DEPOIS
+    intro.executar()
     relogio = Relogio()
-    nome = input('Digite o nome do seu cão amigo(a): ')
-    personagem = Personagem(nome)
+    personagem = Personagem('Gaara')
+    carrocinha = Carrocinha()
     fase1(relogio, personagem)
-
