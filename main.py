1d9a4a7a94d9c4d70b9211335846815ce2c13611
ivancoruja <ivanvz@gmail.com>
2021-07-07 21:25:01 -0300

    ajustes

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     intro = Introducao()
     # intro.executar()   ####NÃO ESQUECER DE TIRAR DE COMENTARIO DEPOIS
     relogio = Relogio()
-    nome = input('Digite o nome do seu cãozinho: '). title()
+    nome = input('Digite o nome do seu cãozinho: ').title()
     personagem = Personagem(nome)
     fase1(relogio, personagem)
